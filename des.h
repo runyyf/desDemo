5b8ded76812d67ad1d27ecd7ba8cfb57c3309c36
runyyf <38844996@qq.com>
2016-09-22 15:10:44 +0800

    add code file

---
+++ des.h
@@ -0,0 +1,38 @@
+/*********************************************************
+ *  des.h
+ *  用户使用des算法头文件
+ *	
+ *********************************************************/
+#ifndef _OPENDESS_H_
+#define _OPENDESS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//ab\0defg
+
+//用户使用的函数
+int DesEnc(
+		unsigned char *pInData,
+		int            nInDataLen,
+		unsigned char *pOutData,
+		int           *pOutDataLen);
+
+//用户使用函数des解密
+int DesDec(
+	   unsigned char *pInData,
+	   int            nInDataLen,
+	   unsigned char *pOutData,
+	   int           *pOutDataLen);
+
+//用户更新8字节加密密钥
+int DesUpdateKey(
+	unsigned char* pUpdateKey);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
